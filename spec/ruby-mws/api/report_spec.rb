8c75639f00682650c1b0d4cd206c5e369f0659b6
Homan Chou <homanchou@gmail.com>
2013-07-02 21:40:08 -0700

    add get report test

--- spec/ruby-mws/api/report_spec.rb
+++ spec/ruby-mws/api/report_spec.rb
@@ -5,7 +5,7 @@ describe MWS::API::Report do
   before :all do
     EphemeralResponse.activate
     @mws = MWS.new(auth_params)
-    @timestamp = "2013-07-01T21:16:51-07:00"
+    @timestamp = "2013-07-02T21:22:25-07:00"
   end
 
   context "requests" do
@@ -19,11 +19,29 @@ describe MWS::API::Report do
 
     describe "poll to see if report is ready" do
       it "should GetReportRequestList by passing a list of report_request_ids" do
-        response = @mws.reports.get_report_request_list :report_request_id_list => ['7454795654','7454842892'], :timestamp => '2013-07-02T00:04:03-07:00'
+        response = @mws.reports.get_report_request_list :report_request_id_list => ['7454795654','7454842892'], :timestamp => @timestamp
         response.report_requests.count.should == 2
       end
 
+      it "should be able to fetch report status" do
+        response = @mws.reports.get_report_request_list :report_request_id_list => ['7461818012'], :timestamp => @timestamp
+        response.report_requests.first.report_processing_status.should == "_DONE_"
+        response.report_requests.first.generated_report_id.should == "11467220403"
+      end
+
     end
+
+    describe "download report" do
+      it "should be able to download the report" do
+        response = @mws.reports.get_report :report_id => '11467220403', :timestamp => @timestamp
+        puts response.inspect
+      end
+
+    end
+
+
+
+
 
 
 
