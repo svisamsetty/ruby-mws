4043e1fa3993df56f90ff417c3012323b2ca9eb9
Homan Chou <homanchou@gmail.com>
2013-07-11 11:32:41 -0700

    add test for _GET_MERCHANT_LISTINGS_DATA_LITE_ and checked parsing of raw tab delimited data

--- spec/ruby-mws/api/report_spec.rb
+++ spec/ruby-mws/api/report_spec.rb
@@ -5,7 +5,7 @@ describe MWS::API::Report do
   before :all do
     EphemeralResponse.activate
     @mws = MWS.new(auth_params)
-    @timestamp = "2013-07-02T21:22:25-07:00"
+    @timestamp = nil #{}"2013-07-11T01:22:25-07:00"
   end
 
   context "requests" do
@@ -16,6 +16,16 @@ describe MWS::API::Report do
         response.report_request_info.should have_key :report_request_id
       end
     end
+
+    describe "request_report" do
+      it "should request report for merchant listings lite" do
+        response = @mws.reports.request_report :report_type => '_GET_MERCHANT_LISTINGS_DATA_LITE_', :timestamp => @timestamp
+        response.report_request_info.should have_key :report_request_id
+      end
+    end
+
+    
+
 
     describe "poll to see if report is ready" do
       it "should GetReportRequestList by passing a list of report_request_ids" do
@@ -29,15 +39,46 @@ describe MWS::API::Report do
         response.report_requests.first.generated_report_id.should == "11467220403"
       end
 
+      it "should GetReportRequestList by passing a list of report_request_ids" do
+        response = @mws.reports.get_report_request_list :report_request_id_list => ['7519546546'], :timestamp => @timestamp
+        response.report_requests.first.generated_report_id.should == "11600899863"
+      end
+
     end
 
     describe "download report" do
       it "should be able to download the report (body is tab delimited data)" do
-        response = @mws.reports.get_report :report_id => '11467220403', :timestamp => @timestamp
+        response = @mws.reports.get_report :report_id => '11467220403', :timestamp => "2013-07-11T10:30:04-07:00"
         response.body.length.should > 0
       end
 
+      it "should be able to download the report (body is tab delimited data)", :focus => true do
+        response = @mws.reports.get_report :report_id => '11600899863', :timestamp => @timestamp
+        response.body.length.should > 0
+
+        #uncomment this to take a look at the tab delimited info in hash form
+        # hash = {}
+        # headers = []
+        # response.body.lines.each_with_index do |line, index|
+        #   if index == 0
+        #     headers = line.strip.split("\t")
+        #   else
+        #     line.split("\t").each_with_index do |v, i|
+        #       puts "#{headers[i]} : #{v}"
+        #     end
+        #   end       
+        # end
+   
+
+      end
+
+
     end
+
+
+
+
+
 
 
 
